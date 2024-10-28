97fa457eea31387eaaa4f0ee28c127417234f213
Enes Çiğdem <enes.cigdem@bidy.com.tr>
2024-10-29 01:16:09 +0300

    Commit 4

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: './app.js',
-  },
+  entry: './app.js',  // Giriş dosyanız
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    filename: './app.js',
-  },
+    path: path.resolve(__dirname, 'public'),  // Çıkış dizini olarak 'public' ayarlayın
+    filename: 'bundle.js'
+  }
 };
